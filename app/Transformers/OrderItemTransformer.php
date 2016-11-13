f946c324bc33249bfb7591da29d464decb9c461b
Luítame de Oliveira <luitamy@hotmail.com>
2016-11-13 16:52:58 -0300

    exemplo de relacionamento aninhado

--- app/Transformers/OrderItemTransformer.php
+++ app/Transformers/OrderItemTransformer.php
@@ -12,6 +12,8 @@ use CodeDelivery\Models\OrderItem;
 class OrderItemTransformer extends TransformerAbstract
 {
 
+    protected $defaultIncludes = ['product'];
+
     /**
      * Transform the \OrderItem entity
      * @param \OrderItem $model
@@ -21,12 +23,17 @@ class OrderItemTransformer extends TransformerAbstract
     public function transform(OrderItem $model)
     {
         return [
-            'id'         => (int) $model->id,
+            'id' => (int)$model->id,
 
             /* place your other model properties here */
 
             'created_at' => $model->created_at,
             'updated_at' => $model->updated_at
         ];
+    }
+
+    public function includeProduct(OrderItem $model)
+    {
+        return $this->item($model->product, new ProductTransformer());
     }
 }
---
+++ app/Transformers/ProductTransformer.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace CodeDelivery\Transformers;
+
+use CodeDelivery\Models\Product;
+use League\Fractal\TransformerAbstract;
+
+/**
+ * Class ProductTransformer
+ * @package namespace CodeDelivery\Transformers;
+ */
+class ProductTransformer extends TransformerAbstract
+{
+
+    /**
+     * Transform the \Product entity
+     * @param \Product $model
+     *
+     * @return array
+     */
+    public function transform(Product $model)
+    {
+        return [
+            'id'         => (int) $model->id,
+            'name'       => (string) $model->name,
+
+            /* place your other model properties here */
+            'created_at' => $model->created_at,
+            'updated_at' => $model->updated_at
+        ];
+    }
+}
