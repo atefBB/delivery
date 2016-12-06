dbc1c734af266b6bddd5fb64e9f16b4e721fc654
Luítame de Oliveira <luitamy@hotmail.com>
2016-12-06 11:36:00 -0300

    ion-refresh

--- ionic/www/js/controllers/client/order.js
+++ ionic/www/js/controllers/client/order.js
@@ -10,12 +10,26 @@ angular
                     template: 'Carregando...'
                 });
 
-                Order.query(
-                    {
+                $scope.doRefresh = function () {
+                    getOrders().then(
+                        function (data) {
+                            $scope.items = data.data;
+                            $scope.$broadcast('scroll.refreshComplete');
+                        }, function (errorResponse) {
+                            $scope.$broadcast('scroll.refreshComplete');
+                        }
+                    );
+                };
+
+                function getOrders() {
+                    return Order.query({
                         id: null,
                         orderBy: 'created_at',
                         sortedBy: 'desc'
-                    },
+                    }).$promise;
+                };
+
+                getOrders().then(
                     function (data) {
                         $scope.items = data.data;
                         $ionicLoading.hide();
