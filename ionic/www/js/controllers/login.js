2d114247022b763015839c4b1486a01e57e64efd
Luítame de Oliveira <luitamy@hotmail.com>
2016-12-09 18:12:30 -0300

    refatorando acesso ao nome do cliente

--- ionic/www/js/controllers/login.js
+++ ionic/www/js/controllers/login.js
@@ -1,19 +1,31 @@
 angular.module('starter.controllers')
-    .controller('LoginCtrl', ['$scope', 'OAuth', '$ionicPopup', '$state', function ($scope, OAuth, $ionicPopup, $state) {
+    .controller('LoginCtrl',
+        ['$scope', 'OAuth', 'OAuthToken', '$ionicPopup', '$state', 'UserData', 'User',
+            function ($scope, OAuth, OAuthToken, $ionicPopup, $state, UserData, User) {
 
-        $scope.user = {
-            username: '',
-            password: ''
-        };
+                $scope.user = {
+                    username: '',
+                    password: ''
+                };
 
-        $scope.login = function () {
-            OAuth.getAccessToken($scope.user).then(function (data) {
-                $state.go('client.checkout');
-            }, function (responseError) {
-                $ionicPopup.alert({
-                    title: 'Aviso',
-                    template: 'Login e/ou senha inválidos!'
-                });
-            });
-        };
-    }]);
+                $scope.login = function () {
+
+                    var promise = OAuth.getAccessToken($scope.user);
+
+                    promise
+                        .then(function (data) {
+                            return User.authenticated({include: 'client'}).$promise;
+                        })
+                        .then(function (dataSuccess) {
+                            UserData.set(dataSuccess.data);
+                            $state.go('client.checkout');
+                        }, function (errorResponse) {
+                            UserData.set(null)
+                            OAuthToken.removeToken();
+                            $ionicPopup.alert({
+                                title: 'Aviso',
+                                template: 'Login e/ou senha inválidos!'
+                            });
+                        });
+                };
+            }]);
