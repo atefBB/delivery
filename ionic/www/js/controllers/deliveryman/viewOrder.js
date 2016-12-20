46619bca060b753578cef53d287c93e0c842b5da
Luítame de Oliveira <luitamy@hotmail.com>
2016-12-20 02:46:57 -0300

    criando update status e geo no resource de order

--- ionic/www/js/controllers/deliveryman/viewOrder.js
+++ ionic/www/js/controllers/deliveryman/viewOrder.js
@@ -10,15 +10,19 @@ angular
                     template: 'Carregando...'
                 });
 
-                DeliverymanOrder.get(
-                    {
-                        id: $stateParams.id,
-                        include: 'items,cupom'
-                    },
-                    function (data) {
-                        $scope.order = data.data;
-                        $ionicLoading.hide();
-                    }, function (errorResponse) {
-                        $ionicLoading.hide();
-                    });
+                DeliverymanOrder.get({id: $stateParams.id, include: 'items,cupom'}, function (data) {
+                    $scope.order = data.data;
+                    $ionicLoading.hide();
+                }, function (errorResponse) {
+                    $ionicLoading.hide();
+                });
+
+                DeliverymanOrder.updateStatus({id: $stateParams.id}, {status: 1}, function (dataSuccess) {
+                    console.log(dataSuccess);
+                });
+
+                DeliverymanOrder.geo({id: $stateParams.id}, {lat: -23.444, long: -45.444}, function (dataSuccess) {
+                    console.log(dataSuccess);
+                });
+
             }]);
